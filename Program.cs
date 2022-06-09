2dfc739072e0215285e3f7a4fbaeaa5c064b23cc
Marcello-Cruz <marcelinhopastelzinho@gmail.com>
2022-06-09 20:32:49 -0300

    add musica com cores no refrão

---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿Console.writeline ("Quando que cê vai aprender a sincronizar a sua sample com sua bateria, mano?");
+Console.writeline ("O bagulho parece que-, que foi feito de-, de olho fechado, de... De nariz fechado não, mano");
+Console.writeline ("Isso parece que foi feito de olho fechado, parece que foi feito com o ouvido tapado");
+Console.writeline ("Parece que cê bateu os dedos no teclado, mano, que que tá acontecendo, meu amigo? Pelo amor de Deus, hein");
+Console.writeline ("Bife, mеlhore");
+Console.writeline ("Celo, você não é tão foda quanto o Bife");
+Console.writeline ("Cala-, cala-");
+
+console.readline()
+
+Console.writeline ("Cala a boca quе o Bife é muito pica (Muito pica)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho (Ha, ha, ha, ha)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Cala-, cala-, cala a boca que o Bife é muito pica (Muito pica)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)"foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho (Vai dar trabalho)"foreground color = ConsoleColor.Violet);
+
+console.readline()
+
+Console.writeline ("Desde que o Bife entrou pro game, o Dr. Dre chorou");
+Console.writeline ("Até o Metro Boomin, mano, ahn, he wants some more");
+Console.writeline ("Kha-, Khaled quis uma selfie quando ele escutou");
+Console.writeline ("No primeiro beat até o 'Ye ejaculou");
+Console.writeline ("Mano, não dá fora no meu produtor (Jamais)");
+Console.writeline ("Ou nois descobre tua residência (Rapapapapapapapa)");
+Console.writeline ("Cola no teu bloco, só os atirador");
+Console.writeline ("O Bife é o mais foda, provamo' com a ciência (H2O na xota delas)");
+Console.writeline ("Quem não curte o Bife vai tomar soco no olho");
+Console.writeline ("Bife tem o molho e eu não tenho paciência");
+Console.writeline ("Fia da puta, num abre a boca, já parto pra violência");
+Console.writeline ("Eu parto essa tua cara porque contra o Bife não tem concorrência (Prra, prra, prra, prra, uh)");
+Console.writeline ("Meu mano, Bife já comeu até tua mãe (Até tua mãe)");
+Console.writeline ("E vai comer de novo, esconde ela, mano (Esconde ela)");
+Console.writeline ("Bife já tirou tua mãe pra sugar baby");
+Console.writeline ("Bife criou o ditado: caiu na rede é peixe");
+
+console.readline()
+
+Console.writeline ("Cala a boca que o Bife é muito pica (Muito pica)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho (Ha, ha, ha, ha)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Cala-, cala-, cala a boca que o Bife é muito pica (Muito pica)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)" foreground color = ConsoleColor.Violet);
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho" foreground color = ConsoleColor.Violet);
+
+console.readline()
+
+Console.writeline ("Aí Tiba, vou ali mijar, fala um pouco do Bife aí");
+Console.writeline ("Aí PMM, essa é pra você, hein (Chupa)");
+Console.writeline ("Han, vai se fuder, tá ligado?");
+Console.writeline ("Vai se f-, vai se f-, vai se fuder, vai se fuder");
+Console.writeline ("Bife é melhor que você" foreground color = ConsoleColor.Violet); 
+Console.writeline ("Vai se fuder, vai se fuder" foreground color = ConsoleColor.Violet);
+Console.writeline ("Bife é melhor que você" foreground color = ConsoleColor.Violet);
+Console.writeline ("An (Uh)");
+Console.writeline ("Yeah (Ah)");
+Console.writeline ("Aham, aham (Aham)");
+Console.writeline ("Chega nessa, chega nessa (Tô aqui já, porra, tô aqui já, mano)");
+
+console.readline()
+
+Console.writeline ("Bife rimou, seu tempo acabou");
+Console.writeline ("Não chama de Bife, chama de doutor");
+Console.writeline ("Se é na música, ele é produtor");
+Console.writeline ("Chega na vida, ele é o terror (Rapapapapapapa)");
+Console.writeline ("Quente igual sauna, drip bem hard");
+Console.writeline ("Beat que toca até na sua alma");
+Console.writeline ("Pegando fogo igual Charizard");
+Console.writeline ("Se vê o Bife, bate até palma");
+Console.writeline ("Vou terminando a música aqui");
+Console.writeline ("Deu pra entender, não vou traduzir");
+Console.writeline ("Se tem o Bife, sabe que é hit");
+Console.writeline ("Nós tamo junto lançando a mais lit, ya ya");
+
+console.readline()
+
+Console.writeline ("Cala a boca que o Bife é muito pica (Muito pica)");
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)");
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)");
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho (Ha, ha, ha, ha)");
+Console.writeline ("Cala-, cala-, cala a boca que o Bife é muito pica (Muito pica)");
+Console.writeline ("Quem não curte, vai pra casa do caralho (Vai pra casa do caralho)");
+Console.writeline ("Tua irmã, quando vê o Bife, quica (Ela vem e quica)");
+Console.writeline ("Tua mãe, o Bife já vai dar trabalho");
